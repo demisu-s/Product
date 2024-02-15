006bfc60f888e752fdb9154734131cb26b435454
demisu-s <damisusefu@gmail.com>
2024-02-15 10:53:20 +0300

    error fix

--- ProductApp/controllers/UserController.js
+++ ProductApp/controllers/UserController.js
@@ -1,12 +1,12 @@
 const Joi = require("joi");
 const asyncHandler = require("express-async-handler");
-const bcrypt = require("bcrypt"); 
-const jwt = require("jsonwebtoken"); 
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../model/userModel");
 
 
 const register = async (req, res) => {
-  const { userName, email, password,role } = req.body;
+  const { userName, email, password, role } = req.body;
 
 
   const salt = await bcrypt.genSalt(10);
@@ -22,35 +22,27 @@ const register = async (req, res) => {
   res.status(201).json(user);
 };
 
-
-const login =async (req, res) => {
+const login = async (req, res) => {
   const { email, password } = req.body;
-
-
-  const user = await User.findOne({ email });
-
- 
-  if (user && (await bcrypt.compare(password, user.password))) {
+ const user = await User.findOne({ email });
+ if (user && (await bcrypt.compare(password, user.password))) {
     const token = generateToken(user.id);
     res.json({
       _id: user.id,
       userName: user.userName,
       email: user.email,
-      token:generateToken(user._id) 
+      token: generateToken(user._id)
     });
   } else {
     res.status(400).json({ error: "Invalid credentials" });
   }
 };
-
-
 const getProfile = (req, res) => {
   res.json(req.user);
 };
 
-
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, 
+  return jwt.sign({ id }, process.env.JWT_SECRET,
     { expiresIn: "30d" });
 };
 
