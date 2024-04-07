8952ba5fc7d937f1912b24dba8d3135c3bddc688
demisu-s <damisusefu@gmail.com>
2024-04-07 10:57:51 +0300

    error fix

--- ProductApp/middleware/UserValidator.js
+++ ProductApp/middleware/UserValidator.js
@@ -5,7 +5,7 @@ const userValidation = (req, res, next) => {
         userName: Joi.string().max(20).min(5).pattern(new RegExp('^[a-zA-Z]+$')),
         email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
         password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-        role:Joi.string(["admin","user"]).default='user'
+        
         // repeat_password: Joi.ref('password'),
         // access_token: [Joi.string(), Joi.number()],
     });
